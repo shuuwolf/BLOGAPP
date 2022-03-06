47b323a1925081bdf633505c3c4f67d70d158833
Rafael Kauch <shuuwolf_second@outlook.com>
2022-03-06 18:47:47 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+    const express = require('express');
+    const handlebars = require('express-handlebars');
+    const bodyParser = require("body-parser")
+    const app = express();
+    const admin = require("./routes/admin")
+    //const mongoose = require("mongoose")
+//Configurações
+    //Body Parser
+        app.use(express.json());
+        app.use(express.urlencoded({extended:true}))
+    // Handlebars
+        app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}))
+        app.set('view engine', 'handlebars')
+    // Mongoose
+    // Public
+    var path = require('path');
+    app.use(express.static(path.join(__dirname, 'public')));
+//Rotas
+    app.get('/', (req, res) => {
+        console.log('Pagina inicial');
+    })
+
+    app.get('/posts', (req, res) => {
+        console.log('Lista de posts');
+    })
+
+    app.use('/admin', admin);
+//Outros
+const PORT = 8081
+app.listen(PORT,() => {
+    console.log("Servidor rodando!")
+})
