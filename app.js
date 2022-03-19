d830f4690ec9b0430f03a2241a34b0c0d59e6d89
Rafael Kauch <shuuwolf_second@outlook.com>
2022-03-19 07:28:34 -0300

    listar postagens, edita-las.

--- app.js
+++ app.js
@@ -4,7 +4,22 @@
     const app = express();
     const admin = require("./routes/admin")
     const mongoose = require("mongoose")
+    const session = require("express-session")
+    const flash = require("connect-flash")
 //Configurações
+    // Sessão
+        app.use(session({
+            secret: "cursodenode",
+            resave: true,
+            saveUninitialized: true
+        }))
+        app.use(flash())
+    // Middleware
+        app.use((req, res, next ) => {
+            res.locals.success_msg = req.flash("success_msg");
+            res.locals.error_msg = req.flash("error_msg");
+            next();//Dar continuidade as requisições, sem o next ira ficar travado no middleware.
+        })
     //Body Parser
         app.use(express.json());
         app.use(express.urlencoded({extended:true}))
@@ -21,11 +36,6 @@
     // Public
     var path = require('path');
     app.use(express.static(path.join(__dirname, 'public')));
-    // Middleware
-        app.use((req, res, next ) => {
-            //code here
-            next();//Dar continuidade as requisições, sem o next ira ficar travado no middleware.
-        })
 //Rotas
     app.get('/', (req, res) => {
         console.log('Pagina inicial');
