4a63f5ab304b43c12fca69e658114833b1e5ab62
Rafael Kauch <shuuwolf_second@outlook.com>
2022-03-21 13:05:15 -0300

    criando sistema de registro, usando Bcrypy.js na senha

--- app.js
+++ app.js
@@ -6,6 +6,11 @@
     const mongoose = require("mongoose")
     const session = require("express-session")
     const flash = require("connect-flash")
+    require("./models/Postagem")
+    const Postagem = mongoose.model("postagens")
+    require("./models/Categoria")
+    const Categoria = mongoose.model("categorias")
+    const usuarios = require("./routes/usuario")
 //Configurações
     // Sessão
         app.use(session({
@@ -38,14 +43,63 @@
     app.use(express.static(path.join(__dirname, 'public')));
 //Rotas
     app.get('/', (req, res) => {
-        console.log('Pagina inicial');
+        Postagem.find().populate("categoria").sort({data: "desc"}).lean().then((postagens) => { //Precisa do lean()
+            res.render("index", {postagens: postagens})
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro ao listar as postagens")
+            res.redirect("/404");
+        })
     })
 
-    app.get('/posts', (req, res) => {
-        console.log('Lista de posts');
+    app.get("/postagem/:slug", (req, res) => {
+        Postagem.findOne({slug: req.params.slug}).lean().then((postagem) => {
+            if(postagem){
+                res.render("postagem/index", {postagem: postagem})
+            }else{
+                req.flash("error_msg", "Esta postagem não existe")
+                res.redirect("/")
+            }
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro interno")
+            res.redirect("/")
+        })
     })
+
+    app.get("/categorias", (req, res) => {
+        Categoria.find().lean().then((categorias) => {
+            res.render("categorias/index", {categorias: categorias})
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro ao listar as categorias")
+            res.redirect("/")
+        })
+    })
+
+    app.get("/categorias/:slug", (req, res) => {
+        Categoria.findOne({slug: req.params.slug}).lean().then((categoria) => {
+            if(categoria){
+                Postagem.find({categoria: categoria._id}).lean().then((postagens) => {
+                    res.render("categorias/postagens", {postagens: postagens, categoria: categoria})
+                }).catch((err) => {
+                    req.flash("error_msg", "Houve um erro ao listar os posts!")
+                    res.redirect("/")
+                })
+            }else{
+                req.flash("error_msg", "Esta categoria nao existe!")
+                res.redirect("/")
+            }
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro interno ao carregar a página desta categoria!")
+            res.redirect("/")
+        })
+    })
+
+    app.get("/404", (req, res) => {
+        res.send("Erro 404!")
+    })
+
 
     app.use('/admin', admin);
+    app.use('/usuarios', usuarios);
 //Outros
 const PORT = 8081
 app.listen(PORT,() => {
--- routes/admin.js
+++ routes/admin.js
@@ -237,4 +237,14 @@ router.post("/postagem/edit", (req, res) => {
     })
 })
 
+router.get("/postagens/deletar/:id", (req, res) => {
+    Postagem.remove({_id: req.params.id}).then(() => {
+        req.flash("success_msg", "Postagem deletada com sucesso!")
+        res.redirect("/admin/postagens")
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro ao deletar a postagem!")
+        res.redirect("/admin/postagens")
+    })
+})
+
 module.exports = router;
